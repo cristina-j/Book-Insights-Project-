7b64fba0d7b8604b0890608563011621f43d8566
Cristina John <cristinajohn60@gmail.com>
2025-07-13 21:54:40 -0400

    books_datatset.py
    Renamed the title of this file and also added a comment to line 8 about the csv file name change in line 9. The name of this file was "books_import_genre_column_updated.py".
    
    The csv file was changed from "books_import_genre_column_updated.csv" to "books_dataset.csv".

--- books_import_genre_column_updated.py
+++ books_import_genre_column_updated.py
@@ -5,9 +5,9 @@ import numpy as np
 import sqlite3
 
 # Read the corrected CSV with updated genre info
-df_books = pd.read_csv("books_genre_column_updated.csv")
-
-# Optional: Reorder columns if needed
+# Updated the csv file name to make it shorter and easier to remember since I have made more than one specific change so far
+df_books = pd.read_csv("books_dataset.csv")
+# Reorder columns if needed. This is optional but it also reassures that this needs to be the order
 df_books = df_books[['Book_ID', 'Title', 'Author', 'Genre', 'Price', 'Published_Date', 'Format']]
 
 # Connect to SQLite database
