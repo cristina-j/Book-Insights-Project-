a0302cde0e7d024f24a48538bbe3558811fc01ca
Cristina John <cristinajohn60@gmail.com>
2025-07-13 21:55:13 -0400

    books_import_genre_column_updated.py
    Added a space in between lines 9 and 11

--- books_import_genre_column_updated.py
+++ books_import_genre_column_updated.py
@@ -7,6 +7,7 @@ import sqlite3
 # Read the corrected CSV with updated genre info
 # Updated the csv file name to make it shorter and easier to remember since I have made more than one specific change so far
 df_books = pd.read_csv("books_dataset.csv")
+
 # Reorder columns if needed. This is optional but it also reassures that this needs to be the order
 df_books = df_books[['Book_ID', 'Title', 'Author', 'Genre', 'Price', 'Published_Date', 'Format']]
 
